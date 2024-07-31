5304011c2dbcfbb2ac5aee30dc68fa6ed85d040d
Hailey Eckstrand <Hailey.Eckstrand@gov.bc.ca>
2024-07-31 11:11:18 -0700

    Altered linear_weight to take in more arguments and moved into functions scripts

--- src/utils/functions.R
+++ src/utils/functions.R
@@ -1,4 +1,157 @@
 library(dadmtools)
+library(dplyr)
+
+linear_weight <- function(
+                               template_tif      = "data\\input\\bc_01ha_gr_skey.tif",  ## "S:\\FOR\\VIC\\HTS\\ANA\\workarea\\PROVINCIAL\\bc_01ha_gr_skey.tif"
+                               mask_tif          = "data\\input\\BC_Boundary_Terrestrial.tif", ## 'S:\\FOR\\VIC\\HTS\\ANA\\workarea\\PROVINCIAL\\BC_Boundary_Terrestrial.tif',
+                               crop_extent       = c(273287.5,1870587.5,367787.5,1735787.5),
+							   grid_tbl          = "whse_sp.nts_50k_grid",
+							   grid_loop_fld     = "map_tile",
+							   grid_geom_fld     = "geom",
+							   dst_schema        = "whse",
+							   dst_tbl           = "bc_linear_features",
+							   pg_conn_param     = pg_conn_param,
+							   create_vector_lyr = TRUE,
+							   spatial_query,
+							   spatial_query_when_error,
+							   tbl_comment
+)
+{
+	script_start_time <- Sys.time()
+	print(glue("Script started at {format(script_start_time, '%Y-%m-%d %I:%M:%S %p')}"))
+
+	conn <- DBI::dbConnect(pg_conn_param["driver"][[1]],
+					host     = pg_conn_param["host"][[1]],
+					user     = pg_conn_param["user"][[1]],
+					dbname   = pg_conn_param["dbname"][[1]],
+					password = pg_conn_param["password"][[1]],
+					port     = pg_conn_param["port"][[1]])
+
+	query <- glue("CREATE TABLE IF NOT EXISTS {dst_schema}.{dst_tbl}_status (
+		{grid_loop_fld} character varying(32) NOT NULL PRIMARY KEY,
+		status text NOT NULL default 'ready',
+		modified_at timestamp with time zone DEFAULT now());")
+	run_sql_r(query, pg_conn_param)
+
+	query <- glue("INSERT INTO {dst_schema}.{dst_tbl}_status ({grid_loop_fld})
+				SELECT
+					grid.{grid_loop_fld}
+				FROM
+					{grid_tbl} grid
+				LEFT JOIN
+					{dst_schema}.{dst_tbl}_status status
+				USING ({grid_loop_fld})
+					WHERE status.{grid_loop_fld} is null;
+				")
+	run_sql_r(query, pg_conn_param)
+
+
+	query <- glue("CREATE TABLE IF NOT EXISTS {dst_schema}.{dst_tbl} (
+		gr_skey INTEGER NOT NULL PRIMARY KEY,
+		fact numeric NOT NULL,
+		{grid_loop_fld} character varying(32) NOT NULL);")
+	run_sql_r(query, pg_conn_param)
+	
+	## retrieve map tiles to loop over from status table
+	query <- glue("SELECT {grid_loop_fld} FROM {dst_schema}.{dst_tbl}_status where status = 'ready'")
+	grid_df <- sql_to_df(query, pg_conn_param)
+	if (nrow(grid_df) < 1) {
+		print(glue("No records in {dst_schema}.{dst_tbl}_status where status = 'ready'"))
+		return()
+	}
+	## create a terra extent object
+	terra_extent <- terra::ext(crop_extent[1], crop_extent[2], crop_extent[3], crop_extent[4])
+	print(glue('Reading in raster: {template_tif}'))
+	template_rast <- terra::rast(template_tif)
+	template_raster_datatype <- datatype(template_rast)
+	
+	print(glue('Reading in raster: {mask_tif}'))
+	mask_rask <- terra::rast(mask_tif)
+
+	rast_lift <- list(template_rast, mask_rask)
+	print(glue('Cropping gr_skey grid and mask to BC extent...'))
+	crop_list <- lapply(rast_lift, function(x){
+			crs(x) <-  "epsg:3005"
+			terra::crop(x, terra_extent, datatype='INT4S')
+			}
+		)
+	## reassign newly cropped layers to original variable
+	template_rast <- crop_list[[1]]
+  	mask_rask <- crop_list[[2]]
+
+	## Create a new masked gr_skey raster
+	gr_skey_rast <- terra::mask(template_rast, mask_rask, datatype = template_raster_datatype)
+
+	## release large rasters from memory
+	template_rast <- NULL
+	mask_rask <- NULL
+	
+	for (i in 1:nrow(grid_df)) {
+		iteration_start_time <- Sys.time()
+		grid_row <- grid_df[i, ]
+		all_rows <- nrow(grid_df)
+		print(glue("On map tile: {grid_row}, {i}/{all_rows}, {format(iteration_start_time, '%Y-%m-%d %I:%M:%S %p')}"))
+
+		tryCatch({
+			vect <- st_cast(st_read(conn, query = glue(spatial_query), crs = 3005), "MULTIPOLYGON")
+		}, error = function(e){
+			## in the case of an error - wrap a buffer within 0.0001 width to 'fix'
+			vect <- st_cast(st_read(conn, query = glue(spatial_query_when_error), crs = 3005), "MULTIPOLYGON")
+		})
+		if (nrow(vect) < 1) {
+			## skip to the next map tile if no results were found within the mapsheet
+			query <- glue("UPDATE {dst_schema}.{dst_tbl}_status SET status = 'completed', modified_at = now() WHERE {grid_loop_fld} = '{grid_row}'")
+			run_sql_r(query, pg_conn_param)
+			print(glue('No records inserted for map tile: {grid_row}'))
+			next
+		}
+
+		vect_extent <- terra::ext(vect)
+		rast_clipped <- terra::crop(gr_skey_rast, vect_extent)
+		## terra extract link:
+		## https://www.paulamoraga.com/book-spatial/the-terra-package-for-raster-and-vector-data.html
+		results <- terra::extract(rast_clipped, vect, weights = TRUE, na.rm = TRUE)
+		## within the results, records sometimes exist where bc_01ha_gr_skey IS NULL
+		## this happen on the coast when the raster has been masked but the linear features 
+		## exists outside the mask
+		## They are not needed, remove records with NULL values in bc_01ha_gr_skey
+		results <- results[complete.cases(results$bc_01ha_gr_skey), ]
+		sum_weight_by_bc_01ha_gr_skey <- results %>%
+			group_by(bc_01ha_gr_skey) %>%
+			summarise(fact = sum(weight))
+		colnames(sum_weight_by_bc_01ha_gr_skey) <- c('gr_skey', 'fact')
+		## write results to a temporary table
+		df_to_pg(Id(schema = dst_schema, table = glue('{dst_tbl}_tmp')), sum_weight_by_bc_01ha_gr_skey, pg_conn_param, overwrite=TRUE)
+		query <- glue("INSERT INTO {dst_schema}.{dst_tbl} AS d (gr_skey, fact, {grid_loop_fld})
+		SELECT
+			gr_skey,
+			fact,
+			'{grid_row}'::text as {grid_loop_fld}
+		FROM 
+			{dst_schema}.{dst_tbl}_tmp
+		ON CONFLICT (gr_skey)
+		DO UPDATE set fact = 
+		CASE 
+			-- when insert has already happened, overwrite, otherwise add
+			WHEN d.{grid_loop_fld} = EXCLUDED.{grid_loop_fld} THEN EXCLUDED.fact
+			ELSE EXCLUDED.fact + d.fact
+		END;")
+		run_sql_r(query, pg_conn_param)
+		query <- glue("UPDATE {dst_schema}.{dst_tbl}_status SET status = 'completed', modified_at = now() WHERE {grid_loop_fld} = '{grid_row}'")
+		run_sql_r(query, pg_conn_param)
+	}
+	## build a helpful table comment
+	today_date <- format(Sys.time(), "%Y-%m-%d %I:%M:%S %p")
+	if (!(is.null(tbl_comment))) {
+		run_sql_r(glue(tbl_comment), pg_conn_param)
+	}
+	run_sql_r(glue("DROP TABLE IF EXISTS {dst_schema}.{dst_tbl}_tmp;"), pg_conn_param)
+	end_time <- Sys.time()
+	duration <- round(difftime(end_time, script_start_time, units = "mins"), 2)
+	print(glue('Script finished. Duration: {duration} minutes.'))
+}
+
+
 import_bcgw_to_pg <- function(
                                src_schema    = "WHSE_BASEMAPPING",
                                src_layer     = "GBA_RAILWAY_TRACKS_SP",
